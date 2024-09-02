8b2d49097ab10d1825c154fabd8ec906482e26eb
SanthiVanguri <vangurisanthi279@gmail.com>
2024-09-02 19:52:08 +0000

    updated Mentors.jsx

--- src/components/Mentors.jsx
+++ src/components/Mentors.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export const Mentors = () => {
     return (
         <div className="flex w-full  items-center justify-center bg-[#02030e]">
-            <div className="flex flex-col w-full max-w-[1140px] h-[450px]  ">
+            <div className="flex flex-col w-full max-w-[1170px] h-[470px]  ">
                 {/* Text part  */}
                 <div className="flex flex-col text-center items-center justify-center">
                 <h1 className="text-[32px] font-semibold text-[#c9c6ec] mb-3">Meet Our Mentors</h1>
@@ -12,7 +12,7 @@ export const Mentors = () => {
                 professional courses at YHills.</p>
             </div>
                 {/* Image part */}
-                <div className="flex flex-row items-center justify-evenly pt-5">
+                <div className="flex flex-row items-center justify-evenly pt-[51px]">
                     <div className="relative flex flex-col w-[220px] bg-[#02030e] rounded-lg h-[275px] items-center justify-center">
                         <Image src="/Garima-Mittal.webp.svg" alt="arrow" height={110} width={110} className='absolute top-0 z-[5]' />
 
@@ -27,12 +27,12 @@ export const Mentors = () => {
                         </div>
                     </div>
                     <div className="relative flex flex-col w-[220px] bg-[#02030e] rounded-lg h-[275px] items-center justify-center">
-                        <Image src="/Garima-Mittal.webp.svg" alt="arrow" height={110} width={110} className='absolute top-0 z-[5]' />
+                        <Image src="/Mentor-3-2-2.webp.svg" alt="arrow" height={110} width={110} className='absolute top-0 z-[5]' />
 
                         <div className="absolute flex flex-col w-[220px] min-h-[220px] bg-[#5743E2] rounded-lg bottom-0 pt-[30px] items-center justify-center">
-                            <h2 className="text-[16px] text-white font-semibold pb-2">Garima Mittal</h2>
+                            <h2 className="text-[16px] text-white font-semibold pb-2">Indrajeet Kumar</h2>
                             <div className="w-[160px] border-t border-gray-500 pb-2"></div>
-                            <p className="text-[13px] text-[#F5FF11] pb-2">AutoCAD Trainer </p>
+                            <p className="text-[13px] text-[#F5FF11] pb-2">Full Stack Developer </p>
                             <div className="flex items-center justify-center p-[12px] rounded-lg bg-[#77777742]">
                                 <Image src="/linkedin.svg" alt="arrow" height={25} width={25}  />
 
@@ -40,12 +40,12 @@ export const Mentors = () => {
                         </div>
                     </div>
                     <div className="relative flex flex-col w-[220px] bg-[#02030e] rounded-lg h-[275px] items-center justify-center">
-                        <Image src="/Garima-Mittal.webp.svg" alt="arrow" height={110} width={110} className='absolute top-0 z-[5]' />
+                        <Image src="/Mentor-3-2-1.webp.svg" alt="arrow" height={110} width={110} className='absolute top-0 z-[5]' />
 
                         <div className="absolute flex flex-col w-[220px] min-h-[220px] bg-[#5743E2] rounded-lg bottom-0 pt-[30px] items-center justify-center">
-                            <h2 className="text-[16px] text-white font-semibold pb-2">Garima Mittal</h2>
+                            <h2 className="text-[16px] text-white font-semibold pb-2">Uttam Grade</h2>
                             <div className="w-[160px] border-t border-gray-500 pb-2"></div>
-                            <p className="text-[13px] text-[#F5FF11] pb-2">AutoCAD Trainer </p>
+                            <p className="text-[13px] text-[#F5FF11] pb-2">Data Scientist </p>
                             <div className="flex items-center justify-center p-[12px] rounded-lg bg-[#77777742]">
                                 <Image src="/linkedin.svg" alt="arrow" height={25} width={25}  />
 
@@ -53,12 +53,12 @@ export const Mentors = () => {
                         </div>
                     </div>
                     <div className="relative flex flex-col w-[220px] bg-[#02030e] rounded-lg h-[275px] items-center justify-center">
-                        <Image src="/Garima-Mittal.webp.svg" alt="arrow" height={110} width={110} className='absolute top-0 z-[5]' />
+                        <Image src="/Mentor-3-2-3.webp.svg" alt="arrow" height={110} width={110} className='absolute top-0 z-[5]' />
 
                         <div className="absolute flex flex-col w-[220px] min-h-[220px] bg-[#5743E2] rounded-lg bottom-0 pt-[30px] items-center justify-center">
-                            <h2 className="text-[16px] text-white font-semibold pb-2">Garima Mittal</h2>
+                            <h2 className="text-[16px] text-white font-semibold pb-2">Vaishnavu C V</h2>
                             <div className="w-[160px] border-t border-gray-500 pb-2"></div>
-                            <p className="text-[13px] text-[#F5FF11] pb-2">AutoCAD Trainer </p>
+                            <p className="text-[13px] text-[#F5FF11] pb-2">Cyber Security Consultant </p>
                             <div className="flex items-center justify-center p-[12px] rounded-lg bg-[#77777742]">
                                 <Image src="/linkedin.svg" alt="arrow" height={25} width={25}  />
 
@@ -66,12 +66,12 @@ export const Mentors = () => {
                         </div>
                     </div>
                     <div className="relative flex flex-col w-[220px] bg-[#02030e] rounded-lg h-[275px] items-center justify-center">
-                        <Image src="/Garima-Mittal.webp.svg" alt="arrow" height={110} width={110} className='absolute top-0 z-[5]' />
+                        <Image src="/H-Tittu-Raja-Stephenson.webp.svg" alt="arrow" height={110} width={110} className='absolute top-0 z-[5]' />
 
                         <div className="absolute flex flex-col w-[220px] min-h-[220px] bg-[#5743E2] rounded-lg bottom-0 pt-[30px] items-center justify-center">
-                            <h2 className="text-[16px] text-white font-semibold pb-2">Garima Mittal</h2>
+                            <h2 className="text-[16px] text-white font-semibold pb-2">T. Raja Stephenson</h2>
                             <div className="w-[160px] border-t border-gray-500 pb-2"></div>
-                            <p className="text-[13px] text-[#F5FF11] pb-2">AutoCAD Trainer </p>
+                            <p className="text-[13px] text-[#F5FF11] pb-2">Digital Marketing Expert </p>
                             <div className="flex items-center justify-center p-[12px] rounded-lg bg-[#77777742]">
                                 <Image src="/linkedin.svg" alt="arrow" height={25} width={25}  />
 
